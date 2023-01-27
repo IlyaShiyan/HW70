ee506f05d7f62c1c4d8e4024009699ac6ced7708
Ilya Shiyan <ilya.shiyan97@mail.ru>
2023-01-27 21:25:11 +0300

    1.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.module.Configuration;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("C:/Test").toAbsolutePath();
+        Files.walkFileTree(Path.of("C:/Test"), new MyFileVisitor());
+    }
+}
+
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.getFileName() + ", " + file.toFile().length() + ", " +
+                file.toFile().isDirectory());
+        return FileVisitResult.CONTINUE;
+    }
+
+}
+
