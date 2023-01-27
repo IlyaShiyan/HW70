76bb05aa5fc7b9ae219f7a427387fa5f96040f68
Ilya Shiyan <ilya.shiyan97@mail.ru>
2023-01-27 21:32:09 +0300

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,10 @@
-import java.io.File;
 import java.io.IOException;
-import java.lang.module.Configuration;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Path path = Paths.get("C:/Test").toAbsolutePath();
         Files.walkFileTree(Path.of("C:/Test"), new MyFileVisitor());
     }
 }
